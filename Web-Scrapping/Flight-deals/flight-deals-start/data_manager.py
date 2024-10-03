4b0c06374beeafc46c64e9d0494476dda7b2ae11
Mohamed Hamdy <mohamed.hamdey.hafez@gmail.com>
2024-10-03 10:25:00 +0300

    Flight-deals-projects

---
+++ Web-Scrapping/Flight-deals/flight-deals-start/data_manager.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/ce3b48cc8b53b6223b0b48aa009dd66f/flightDeals/prices"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
